cabe16f5093f486a2dbdff885dd7274e6860cbea
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-07-13 22:05:11 +0330

    implement blog admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Post, Category
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'category', 'status', 'created_date', 'published_date'] 
+    list_editable = ['status']
+    list_per_page = 10
+    list_filter = ['category']
+    search_fields = ['title', 'author', 'content']
+    date_hierarchy = 'created_date'
+    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
