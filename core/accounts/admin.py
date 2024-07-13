8995957e4ac10be6b8ea2adc7d0ff18c02192d44
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-07-13 20:55:34 +0330

    implement user & profile model and admin panel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Profile, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("email", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                )
+            },
+        ),("Group Permissions",
+            {
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                )
+            },),("Important dates",
+            {
+                "fields": (
+                    "last_login",
+                )
+            },),
+    )
+    add_fieldsets = (
+        (
+            "Create",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+        ("User Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            }),
+    )
+
+
+admin.site.register(Profile)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Custom user manager that handles authentication and authorization requests for the given user account.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """Create a new user
+
+        Args:
+            email (email): email address of the new user
+            password (password): hashed password for the new user
+        """
+        if not email:
+            raise ValueError(_('email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+        
+    def create_superuser(self, email, password, **extra_fields): 
+        """Create a new superuser
+
+        Args:
+            email (email): email address of the new superuser
+            password (password): hashed password for the new superuser
+        """
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('staff user must have set is_staff=True')) 
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('superuser must have set is_superuser=True'))
+
+        return self.create_user(email, password, **extra_fields)
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model for accounts app
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    object = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+
+class Profile(models.Model):
+    """Create a new Profile for User
+    
+    Args:
+        first_name (char field)
+        last_name (char field)
+        image (image): profile image
+        description (text): description of the user
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.user.email
+    
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)        
